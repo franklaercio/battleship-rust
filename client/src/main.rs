22493a11ead4489ca80d9bcd0210e5e253391225
Frank <franklaercio@gmail.com>
2024-06-18 22:47:51 -0300

    feat: adding display board

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,18 +1,64 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader, Write};
 use std::net::TcpStream;
 
 fn main() -> io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:8080")?;
     println!("Connected to the server.");
 
-    let mut reader = io::BufReader::new(stream.try_clone()?);
-    let mut buffer = String::new();
+    // Primeiro, o jogador posiciona seus navios
+    // place_ships(&mut stream);
 
-    while reader.read_line(&mut buffer)? > 0 {
-        println!("Server: {}", buffer.trim());
-        buffer.clear();
+    let stream_clone = stream.try_clone()?;
+    std::thread::spawn(move || {
+        let mut reader = BufReader::new(stream_clone);
+        let mut buffer = String::new();
+
+        while reader.read_line(&mut buffer).unwrap() > 0 {
+            let response = std::str::from_utf8(buffer.as_bytes()).unwrap();
+            if response.starts_with("UPDATE_BOARD") {
+                let board_data = response.replace("UPDATE_BOARD;", "");
+                let cleaned_board_data = board_data;
+                display_board(&cleaned_board_data);
+            } else if response.starts_with("CHECK_END_GAME") {
+                // Handle end game check here
+            }
+
+            buffer.clear();
+        }
+    });
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let input = line?;
+        if input == "PLACE_BOMB" {
+            print!("Enter the coordinates to place the bomb: ");
+
+            io::stdout().flush()?;
+            let mut bomb_coords = String::new();
+            io::stdin().read_line(&mut bomb_coords)?;
+            let bombs = format!("PLACE_BOMB;{}", bomb_coords);
+            stream.write(bombs.as_bytes()).unwrap();
+            continue;
+        }
+        stream.write_all(input.as_bytes())?;
+        stream.write_all(b"\n")?;
     }
 
     Ok(())
 }
 
+fn display_board(boards_data: &String) {
+    let boards: Vec<&str> = boards_data.split("||").collect();
+    if boards.len() == 2 {
+        println!("\n        Y O U                   E N E M Y     \n");
+        let your_board: Vec<&str> = boards[0].split(';').collect();
+        let enemy_board: Vec<&str> = boards[1].split(';').collect();
+        for (your_row, enemy_row) in your_board.iter().zip(enemy_board.iter()) {
+            println!("{}    {}", your_row, enemy_row);
+        }
+    }
+}
+
+// fn place_ships(_stream: &mut TcpStream) {
+//     todo!()
+// }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,31 +1,54 @@
-use std::io::{Result, Write};
+use std::io::{Read, Result, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::Mutex;
 
-fn handle_client(mut client1: TcpStream, mut client2: TcpStream) -> Result<()> {
+use crate::board::Board;
+
+mod board;
+
+fn handle_client(mut client1: TcpStream, mut client2: TcpStream, player_board: Board, enemy_board: Board) -> Result<()> {
     writeln!(client1, "Paired with another player!")?;
     writeln!(client2, "Paired with another player!")?;
+
+    let player_board_state = player_board.render();
+    let enemy_board_state = enemy_board.render();
+    let response = format!("UPDATE_BOARD;{}||{}\n", player_board_state, enemy_board_state);
+
+    client1.write(response.as_bytes()).unwrap();
+    client2.write(response.as_bytes()).unwrap();
 
     Ok(())
 }
 
-fn new_client(mut this: TcpStream) -> Result<()> {
+fn new_client(mut stream: TcpStream, player_board: Board, enemy_board: Board) -> Result<()> {
     static GUARD: Mutex<Option<TcpStream>> = Mutex::new(None);
 
-    writeln!(this, "Awaiting another player...")?;
-    println!("Client connected: {:?}", this.peer_addr());
+    writeln!(stream, "Awaiting another player...")?;
+    println!("Client connected: {:?}", stream.peer_addr());
 
     let another = {
         let mut guard = GUARD.lock().unwrap();
         if let Some(another) = guard.take() {
             another
         } else {
-            *guard = Some(this);
+            *guard = Some(stream);
             return Ok(());
         }
     };
 
-    handle_client(this, another)?;
+    // let mut buffer = [0; 512];
+    // stream.read(&mut buffer).unwrap();
+    //
+    // let request = String::from_utf8_lossy(&buffer[..]);
+    // let cleaned_request = request.trim();
+    //
+    // if cleaned_request.starts_with("PLACE_SHIP") {
+    //     todo!()
+    // } else if cleaned_request.starts_with("PLACE_BOMB") {
+    //     todo!()
+    // }
+
+    handle_client(stream, another, player_board, enemy_board)?;
 
     Ok(())
 }
@@ -34,10 +57,13 @@ fn main() -> Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8080")?;
     println!("Server listening on {}", listener.local_addr().unwrap());
 
+    let player_board = Board::new();
+    let enemy_board = Board::new();
+
     for stream in listener.incoming() {
         if let Ok(s) = stream {
-            std::thread::spawn(|| {
-                let _ = new_client(s);
+            std::thread::spawn(move || {
+                let _ = new_client(s, player_board, enemy_board);
             });
         }
     }
